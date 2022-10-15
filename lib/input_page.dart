5d63fbbb241b4e7737d318a7a5c025acc35c68c4
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-15 14:29:18 +0300

    Customizing Flutter widgets using themes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -94,17 +94,27 @@ class _InputPageState extends State<InputPage> {
 
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height=newValue.round();
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbColor: Color(0xFFEB1555),
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 16,),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30,),
+
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height=newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
